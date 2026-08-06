4de516920e31613f8165e83ccef4b3bdeee6a580
didier01 <deramirez19@misena.edu.co>
2026-08-06 09:42:57 -0500

    chore: configure Netlify deployment settings and production environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.alitas.com',
+  useMocks: false,
+  supabaseUrl: 'https://ldxmibnpfapyeqizgyao.supabase.co',
+  supabaseKey: 'sb_publishable_75KTd26aW_uzC7zXVzoQ2Q_FtMFUPrW'
+};
